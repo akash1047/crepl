054b14df1ea7b467545cbbd58d22498e7a2ed1c1
Avideb Mandal <113199484+Ikaris003@users.noreply.github.com>
2024-01-29 23:27:00 +0530

    greeting message is added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     // println!("Hello, world!");
-    greeting();
+    greeting("This is crepl an interpreter for C!");
 }
-fn greeting(){
-    println!("hii");
+fn greeting(msg:&str){
+    
+
+println!("Hi {}! {}", whoami::realname().split(" ").collect::<Vec<_>>()[0], msg);
 }
