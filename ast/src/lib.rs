5631256f211011fe9c8d0dfc4a52cfe89d9d9730
Subham Mandal <138233701+Subham2003-cloud@users.noreply.github.com>
2024-01-31 21:45:37 +0530

    procide further

--- ast/src/lib.rs
+++ ast/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use token::Token;
+struct ReturnStatement{
+    token:Token,
+    expression:Option<Expression>,
+    semicollone:Token,
 }
+struct Expression;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
