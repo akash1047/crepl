f16670da585745287707bff5d32a54829731f7c4
kaustavee ghosh <kaustaveeghosh@gmail.com>
2024-01-31 10:04:43 +0530

    added c operators

--- token/src/lib.rs
+++ token/src/lib.rs
@@ -7,20 +7,91 @@ pub enum Tag {
     IDENT,
     INTEGER,
     STRING,
+    FLOATING,
+    CHARACTER,
+    //OPERATORS
+    PLUS,        // +
+    MINUS,       // -
+    STAR,        // *
+    SLASH,       // /
+    MODULO,      //%
+    LESS_THAN,   // <
+    GREATER_THAN, // >
+    LESS_THAN_EQUAL, // <=
+    GREATER_THAN_EQUAL,  // >=
+    NOT_EQUAL,     // !=
+    EQUALITY,     // ==
+    AND,          // &&
+    OR,           // ||
+    LOGICAL_NOT,  // !
+    BITWISE_AND,  // &
+    BITWISE_OR,  // |
+    BITWISE_XOR, //^
+    SHIFT_LEFT,  // <<
+    SHIFT_RIGHT, // >>
+    BITWISE_NOT, //~
+    INCREMENT, // ++
+    DECREMENT, // --
+    POINTER, //  ->*
+    ADDRESS, //&
+
+
+
 
     ASSIGN,    // =
-    PLUS,      // +
     PLUS_PLUS, // ++
-    LESS_THAN, // <
-
+   //DELIMITERS
     SEMICOLON, // ;
+    COMMA,     //,
+    COLON,     // :
+    DOT,       //.
+    LBRACKET,  //[
+    RBRACKET,  //]
     LPAREN,    // (
     RPAREN,    // )
     LBRACE,    // {
     RBRACE,    // }
-
+  //keywords
     INT,
     FOR,
+    AUTO,
+    DO,
+    IF,
+    ELSE,
+    RETURN,
+    SIZEOF,
+    WHILE,
+    CHAR,
+    CONST,
+    SHORT,
+    VOLATILE,
+    DOUBLE,
+    FLOAT,
+    LONG,
+    SIGNED,
+    UNSIGNED,
+    STATIC,
+    STRUCT,
+    UNION,
+    ENUM,
+    CASE,
+    GOTO,
+    SWITCH,
+    BREAK,
+    DEFAULT,
+    REGISTER,
+    CONTINUE,
+    //COMPOUND ASSIGN
+    INC_ASSIGN, //+=
+    DEC_ASSIGN,   //-=
+    DIV_ASSIGN,  ///=
+    MOD_ASSIGN,  // %=
+    LEFT_SHIFT_ASSIGN, // <<=
+    RIGHT_SHIFT_ASSIGN,// >>=
+    AND_ASSIGN,  //&=
+    XOR_ASSIGN,  //^=
+    OR_ASSIGN,   //|=
+
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -36,6 +107,33 @@ pub fn lookup_ident(ident: &str) -> Tag {
     match ident {
         "int" => Tag::INT,
         "for" => Tag::FOR,
+        "auto" => Tag::AUTO,
+        "do" => Tag::DO,
+        "if" => Tag::IF,
+        "else" => Tag::ELSE,
+        "return" => Tag::RETURN,
+        "sizeof" => Tag::SIZEOF,
+        "while" => Tag::WHILE,
+        "char" => Tag::CHAR,
+        "const" => Tag::CONST,
+        "short" => Tag::SHORT,
+        "volatile" => Tag::VOLATILE,
+        "double" => Tag::DOUBLE,
+        "float" => Tag::FLOAT,
+        "long" => Tag::LONG,
+        "signed" => Tag::SIGNED,
+        "unsigned" => Tag::UNSIGNED,
+        "static" => Tag::STATIC,
+        "struct" => Tag::STRUCT,
+        "union" => Tag::UNION,
+        "enum" => Tag::ENUM,
+        "case" => Tag::CASE,
+        "goto" => Tag::GOTO,
+        "switch" => Tag::SWITCH,
+        "default" => Tag::DEFAULT,
+        "register" => Tag::REGISTER,
+        "break" => Tag::BREAK,
+        "continue" => Tag::CONTINUE,
         _ => Tag::IDENT,
     }
 }
