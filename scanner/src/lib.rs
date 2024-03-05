9621478a405b3ef2dc1d76dca621a7f457bc508e
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2024-03-06 04:41:58 +0530

    scanner refactor 3.0

--- scanner/src/lib.rs
+++ scanner/src/lib.rs
@@ -1,141 +1,147 @@
-use std::path::Prefix;
-
-use token::{Position, Token};
-
-pub struct Error {
-    pub pos: Position,
-    pub msg: String,
-}
-
-type ErrorHandle = Box<dyn Fn(Position, String)>;
+use token::Token;
 
 pub struct Scanner {
     src: String,
 
-    ch: u8,
-    offset: usize,
-    rd_offset: usize,
+    ch: u8,           // currently processing character
+    offset: usize,    // position of the current character
+    rd_offset: usize, // position of the next character
 
-    line_offset: usize,
-    line_no: usize,
-
-    err: ErrorHandle,
+    lines: Vec<usize>, // offset of the lines
 }
 
-impl Scanner {
-    pub fn new(src: String, err: ErrorHandle) -> Self {
+impl From<String> for Scanner {
+    fn from(src: String) -> Self {
         let mut s = Self {
             src,
             ch: b' ',
             offset: 0,
             rd_offset: 0,
-            line_offset: 0,
-            line_no: 1,
-            err,
+            lines: vec![0],
         };
         s.next();
         s
     }
+}
 
+impl Scanner {
     fn next(&mut self) {
         if let Some(&ch) = self.src.as_bytes().get(self.rd_offset) {
             self.offset = self.rd_offset;
+
             if self.ch == b'\n' {
-                self.line_offset = self.offset;
-                self.line_no += 1;
+                self.lines.push(self.offset);
             }
 
-            if self.ch == 0 {
-                self.error(format!("invalid character null"));
-            }
-
-            self.rd_offset += 1;
             self.ch = ch;
+            self.rd_offset += 1;
         } else {
             self.offset = self.src.len();
-            if self.ch == b'\n' {
-                self.line_offset = self.offset;
-                self.line_no += 1;
-            }
             self.ch = 0;
         }
     }
-    fn error(&self, msg: String) {
-        (self.err)(self.position(), msg);
-    }
 
-    fn peek(&self) -> u8 {
+    fn peek(&mut self) -> u8 {
         *self.src.as_bytes().get(self.rd_offset).unwrap_or(&0)
     }
 
-    #[inline]
     fn skip_whitespace(&mut self) {
-        while self.ch.is_ascii_whitespace() {
+        while self.ch == b' ' || self.ch == b'\t' || self.ch == b'\n' || self.ch == b'\r' {
             self.next();
         }
     }
 
-    #[inline]
-    fn position(&self) -> Position {
-        Position {
-            offset: self.offset,
-            line: self.line_no,
-            column: self.offset - self.line_offset + 1,
-        }
-    }
     fn advance(&mut self, len: usize) {
-        self.offset += len;
+        let offset = (self.offset + len).min(self.src.len());
+        self.offset = offset;
         self.rd_offset = self.offset + 1;
-        self.ch = *self.src.as_bytes().get(self.offset).unwrap_or(&0);
+        self.ch = self.src.as_bytes()[offset];
     }
 
-    pub fn switch(&mut self, def: Token, alt: &[(u8, Token)]) -> Token {
-        let peek = self.peek();
+    fn switch(&mut self, def: Token, alts: &[(u8, Token)]) -> Token {
+        let p = self.peek();
 
-        for (ch, tok) in alt {
-            if peek == *ch {
+        for &(c, t) in alts {
+            if c == p {
                 self.next();
-                return *tok;
+                return t;
             }
         }
 
         def
     }
-    fn expect(&mut self, ch: u8, tok: Token) -> Option<Token> {
-        if self.peek() == ch {
-            self.next();
-            return Some(tok);
-        }
 
-        None
-    }
-
-    pub fn scan(&mut self) -> (Token, Position, &str) {
+    pub fn scan(&mut self) -> Result<(Token, usize, &str), (Token, usize, &str, String)> {
         self.skip_whitespace();
 
-        let pos = self.position();
-
-        if is_letter(self.ch) && !is_digit(self.ch) {
-            while is_letter(self.ch) {
-                self.next();
-            }
-
-            let lit = &self.src[pos.offset..self.offset];
-            let tok = token::lookup(lit);
-
-            return (tok, pos, lit);
-        }
-
-        if is_digit(self.ch) {
-            while is_digit(self.ch) {
-                self.next();
-            }
-
-            return (Token::INTEGER, pos, &self.src[pos.offset..self.offset]);
-        }
+        let pos = self.offset;
 
         let tok = match self.ch {
-            b'=' => Token::ASSIGN,
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' | b'$' => {
+                let len = count_if(self.src[pos..].bytes(), is_letter);
+                self.advance(len);
+                let lit = &self.src[pos..pos + len];
+                let tok = token::lookup(lit);
+
+                return Ok((tok, pos, lit));
+            }
+
+            b'0' => {
+                self.next();
+                let prefix = self.ch;
+
+                match prefix {
+                    b'b' => {
+                        self.next();
+
+                        let digits = count_if(self.src[self.offset..].bytes(), is_binary_digit);
+                        self.advance(digits);
+                        let lit = &self.src[pos..self.offset];
+
+                        if digits == 0 {
+                            let msg = format!(
+                                "at least one binary digit required after 0'{}'",
+                                prefix as char
+                            );
+                            return Err((Token::ILLEGAL, pos, lit, msg));
+                        }
+                        return Ok((Token::INTEGER, pos, lit));
+                    }
+
+                    b'x' | b'X' => {
+                        self.next();
+
+                        let digits = count_if(self.src[self.offset..].bytes(), is_hex_digit);
+                        self.advance(digits);
+                        let lit = &self.src[pos..self.offset];
+
+                        if digits == 0 {
+                            let msg = format!(
+                                "at least one hex digit required after 0'{}'",
+                                prefix as char
+                            );
+                            return Err((Token::ILLEGAL, pos, lit, msg));
+                        }
+                        return Ok((Token::INTEGER, pos, lit));
+                    }
+
+                    _ => {
+                        let digits = count_if(self.src[self.offset..].bytes(), is_octal_digit);
+                        self.advance(digits);
+                        let lit = &self.src[pos..self.offset];
+
+                        return Ok((Token::INTEGER, pos, lit));
+                    }
+                }
+            }
+
+            b'1'..=b'9' => {
+                let digits = count_if(self.src[self.offset..].bytes(), is_digit);
+                self.advance(digits);
+                let lit = &self.src[pos..self.offset];
+
+                return Ok((Token::INTEGER, pos, lit));
+            }
 
             b'+' => self.switch(
                 Token::PLUS,
@@ -146,162 +152,88 @@ impl Scanner {
                 Token::MINUS,
                 &[
                     (b'-', Token::DEC),
-                    (b'=', Token::MINUS_ASSIGN),
                     (b'>', Token::ARROW),
+                    (b'=', Token::MINUS_ASSIGN),
                 ],
             ),
 
-            // b'<' => self.switch(Token::LT, &[(b'<', Token::SHL), (b'=', Token::LEQ),]),
-            b'<' => match self.peek() {
-                b'=' => {
-                    self.next();
-                    Token::LEQ
-                }
-                b'<' => {
-                    self.next();
-                    if self.peek() == b'=' {
-                        self.next();
-                        Token::SHL_ASSIGN
-                    } else {
-                        Token::SHL
-                    }
-                }
-                _ => Token::LT,
-            },
-            b'>' => match self.peek() {
-                b'=' => {
-                    self.next();
-                    Token::LEQ
-                }
-                b'>' => {
-                    self.next();
-                    if self.peek() == b'=' {
-                        self.next();
-                        Token::SHR_ASSIGN
-                    } else {
-                        Token::SHR
-                    }
-                }
-                _ => Token::GT,
-            },
-
-            //b'>' => self.switch(Token::GT, &[(b'<', Token::SHR), (b'=', Token::GEQ)]),
             b'*' => self.switch(Token::ASTERISK, &[(b'=', Token::MUL_ASSIGN)]),
             b'/' => self.switch(Token::SLASH, &[(b'=', Token::DIV_ASSIGN)]),
             b'%' => self.switch(Token::REM, &[(b'=', Token::REM_ASSIGN)]),
+
             b'&' => self.switch(
                 Token::AND,
-                &[(b'&', Token::LAND), (b'=', Token::AND_ASSIGN)],
+                &[(b'=', Token::AND_ASSIGN), (b'&', Token::LAND)],
             ),
-            b'|' => self.switch(Token::OR, &[(b'|', Token::LOR), (b'=', Token::OR_ASSIGN)]),
+
+            b'=' => self.switch(Token::ASSIGN, &[(b'=', Token::EQL)]),
+            b'|' => self.switch(Token::OR, &[(b'=', Token::OR_ASSIGN)]),
             b'^' => self.switch(Token::XOR, &[(b'=', Token::XOR_ASSIGN)]),
-            b'!' => self.switch(Token::NOT, &[(b'=', Token::NEQ)]),
 
-            b'.' => {
-                if self.peek() == b'.' {
-                    self.next();
-
-                    if self.peek() == b'.' {
-                        self.next();
-                        Token::ELLIPSE
-                    } else {
-                        Token::ILLEGAL
-                    }
-                } else {
-                    Token::DOT
-                }
-            }
-
-            b'~' => Token::TILDE,
-
-            b'?' => Token::TERNERY,
-            b',' => Token::COMMA,
             b';' => Token::SEMICOLON,
-            b':' => Token::COLON,
-            b'(' => Token::LPAREN,
-            b')' => Token::RPAREN,
-            b'{' => Token::LBRACE,
-            b'}' => Token::RBRACE,
-            b'[' => Token::LBRACK,
-            b']' => Token::RBRACK,
 
-            0 => return (Token::EOF, pos, ""),
-            _ => Token::ILLEGAL,
+            0 => return Ok((Token::EOF, pos, "")),
+            _ => {
+                let ch = self.ch;
+
+                self.next();
+
+                let msg = format!("illegal character '{}' found", ch as char);
+
+                return Err((Token::ILLEGAL, pos, &self.src[pos..self.offset], msg));
+            }
         };
 
         self.next();
 
-        let lit = &self.src[pos.offset..self.offset];
-
-        (tok, pos, lit)
+        Ok((tok, pos, &self.src[pos..self.offset]))
     }
 }
 
 fn is_letter(c: u8) -> bool {
-    c >= b'a' && c <= b'z'
-        || c >= b'A' && c <= b'Z'
-        || c >= b'0' && c <= b'9'
-        || c == b'_'
-        || c == b'$'
+    c.is_ascii_alphanumeric() || c == b'_' || c == b'$'
 }
 
 fn is_digit(c: u8) -> bool {
-    c >= b'0' && c <= b'9'
+    c.is_ascii_digit()
 }
-fn is_hex_digit(c: u8) -> bool {
-    c >= b'0' && c <= b'9' || c >= b'A' && c <= b'F' || c >= b'a' && c <= b'f'
-}
-fn is_octal_digit(c: u8) -> bool {
-    c >= b'0' && c <= b'7'
-}
+
 fn is_binary_digit(c: u8) -> bool {
     c == b'0' || c == b'1'
 }
 
-impl IntoIterator for Scanner {
-    type Item = (Token, Position, String);
-
-    type IntoIter = ScannerIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        Self::IntoIter { scanner: self }
-    }
+fn is_hex_digit(c: u8) -> bool {
+    c.is_ascii_hexdigit()
 }
 
-pub struct ScannerIter {
-    scanner: Scanner,
+fn is_octal_digit(c: u8) -> bool {
+    c >= b'0' && c <= b'7'
 }
 
-impl Iterator for ScannerIter {
-    type Item = (Token, Position, String);
+fn count_if<I: Copy>(src: impl Iterator<Item = I>, check: impl Fn(I) -> bool) -> usize {
+    let mut count = 0;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let (tok, pos, lit) = self.scanner.scan();
+    src.take_while(|i| check(*i)).for_each(|_| count += 1);
 
-        if tok == Token::EOF {
-            return None;
-        }
-
-        Some((tok, pos, lit.to_string()))
-    }
+    count
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use Token::*;
+    use token::Token::*;
 
     #[test]
     fn test_scan() {
         let tests = [
-            (ILLEGAL, "@"),
+            // (ILLEGAL, "@"),
             (IDENT, "intIs_32bit"),
             (IDENT, "_Give_me_100$"),
             (IDENT, "$"),
             (INTEGER, "1234567890"),
-            //(INTEGER, "01234567"),
-            //(INTEGER, "0x123456790abcdefABCDEF"),
-            //(INTEGER, "0b1010"),
+            (INTEGER, "01234567"),
+            (INTEGER, "0x123456790abcdefABCDEF"),
+            (INTEGER, "0b1010"),
             // (FLOATING, "0."),
             // (FLOATING, ".1"),
             // (FLOATING, "3.1"),
@@ -398,10 +330,10 @@ mod tests {
             .collect::<Vec<_>>()
             .join(" ");
 
-        let mut s = Scanner::new(source.clone(), Box::new(|_, _| {}));
+        let mut s = Scanner::from(source.clone());
 
         for (i, t) in tests.iter().enumerate() {
-            let (tok, _, lit) = s.scan();
+            let (tok, _, lit) = s.scan().unwrap();
 
             assert_eq!(
                 *t,
